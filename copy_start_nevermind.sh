7b3921a87a316c85324445cc76ca7b34eb07da3f
epoch <epoch@hacking.allowed.org>
2020-08-06 15:38:31 -0500

    copy_start_nevermind now only shows the user a shortened version, has extra "verbose" option to see whole thing, and a "forget" option which might be flakey if you select an empty URI

--- copy_start_nevermind.sh
+++ copy_start_nevermind.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-scheme="$(uricut -s <<< "$1")"
-selection="$(printf "%s\ncopy\nstart\nnevermind\n" "$(printf "%s\n" "$1" | uricut | grep -v '^whole')" \
-            | choose "$(printf "uri: '%s'\nwill be ran with: %s\n" "$1" "$(urigetline "$1")" )")"
+[ "$1" ] || exit 1
+uri="$1"
+if [ "${uri}" != "${uri:0:64}" ];then
+  prompt="${uri:0:64}..."
+fi
+scheme="$(uricut -s <<< "$uri")"
+selection="$(printf "start\ncopy\nshorten\nverbose\nforget\n%s\n" "$(printf "%s\n" "$uri" | uricut | grep -v '^whole')" \
+             | choose "${prompt}")"
+#            | choose "$(printf "uri: '%s'\nwill be ran with: %s\n" "$uri" "$(urigetline "$uri")" )" -default start )"
 echo "$selection"
+if [ "$selection" = "verbose" ];then
+  x-terminal-emulator -e less -f <(printf "%s" "$uri")
+  exit 0
+fi
 if [ "$selection" = "copy" ];then
-  printf "%s" "$1" | xclip -i
+  printf "%s" "$uri" | xclip -i
+  exit 0
+fi
+if [ "$selection" = "forget" ];then
+  cp ~/.cache/uristart.log ~/.cache/uristart.log.tmp
+  cat ~/.cache/uristart.log.tmp | grep -vFw "${uri}" > ~/.cache/uristart.log
   exit 0
 fi
 if [ "$selection" = "start" ];then
-  scheme="$(printf "%s\n" "$1" | uricut -s)"
-  exec uristart "$1" "$(xdotool getwindowfocus getwindowname)"
+  scheme="$(printf "%s\n" "$uri" | uricut -s)"
+  exec uristart "$uri" "$(xdotool getwindowfocus getwindowname)"
   exit 0
 fi
-exec printf "%s" "$selection" | cut '-d ' -f2- | xclip -i
+if [ "$selection" = "shorten" ];then
+  escaped="$(uriescape "$uri")"
+  gemini-get "gemini://epo.k.vu/submit?$escaped" | tr -d '\r' | tail -n+2 | head -n2 | tr '\n' '\0' | xargs -n1 -0 copy_start_nevermind.sh
+  exit 0
+fi
+if [ "$selection" ];then
+  exec printf "%s" "$selection" | cut '-d ' -f2- | xclip -i
+fi
